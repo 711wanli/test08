65cd8590f154f6465a5fa324c4f1e397f61ffba1
711wanli <641444821@qq.com>
2018-07-01 21:40:44 +0800

    first commit

--- scripts/test_dispaly.py
+++ scripts/test_dispaly.py
@@ -1,7 +1,13 @@
 class TestDisplay:
 
     def setup(self):
-        print ("哈哈")
+        print ("哈")
 
     def test_display_search(self):
         print("hehe")
+
+    def test_display_search1(self):
+        print("h解放路上课")
+
+    def test_display_search2(self):
+        print("h解放路上课")
